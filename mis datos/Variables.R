661af525ece750b941aeb311b7655d78fdaf96ef
armandomaxfli <armando.mf6@gmail.com>
2023-03-17 10:33:28 -0700

    Create Variables.R

---
+++ mis datos/Variables.R
@@ -0,0 +1,36 @@
+pi
+pi^2
+#para crear una variable es necesario poner el nombre de la variable
+#seguido del signo igual, la variable la podemos ver en la zona de environment
+
+x = pi^2
+x
+#tambien se puede hacer una variable haciendo una flecha
+y <- cos(pi/4)
+
+sin(pi/4) -> z
+
+edad <- 30
+
+nombre = "Armando Mendoza"
+
+#R puede distinguir entre mayusculas y minusculas para crear variables
+
+HOLA = 1
+hola = 5
+
+hola
+HOLA
+
+#las variables siempre deben de empezar con una letra
+pi.4 = 4*pi
+pi.4
+
+#siempre asignamos un valor a una variable
+#pero una variable puede cambiar de valor
+
+x = 2
+x = x^2
+
+x = sqrt(x)
+
